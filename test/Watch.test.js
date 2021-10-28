89baeb846810369f2fd440d14fcefb34a18e24fe
Le Vivilet <levivlet@pm.me>
2021-10-28 22:24:06 +0200

    add test for when file is created

--- test/Watch.test.js
+++ test/Watch.test.js
@@ -1,7 +1,8 @@
-import { mkdtemp } from "fs/promises";
+import { mkdtemp, writeFile } from "fs/promises";
 import { tmpdir } from "os";
 import { join } from "path";
 import { watch } from "../src/parts/Watch";
+import waitForExpect from "wait-for-expect";
 
 const getTmpDir = () => {
   return mkdtemp(join(tmpdir(), "foo-"));
@@ -26,3 +27,24 @@ test("no folder given", async () => {
 });
 
 // TODO test invalid arguments
+
+test("listen - file created", async () => {
+  const events = [];
+  const tmpDir = await getTmpDir();
+  const watcher = await createWatcher(tmpDir);
+  watcher.on("all", (event) => events.push(event));
+  await writeFile(`${tmpDir}/a.txt`, "");
+  await waitForExpect(() => {
+    expect(events).toEqual([
+      {
+        operation: "CREATE",
+        path: `${tmpDir}/a.txt`,
+      },
+      {
+        operation: "CLOSE_WRITE",
+        path: `${tmpDir}/a.txt`,
+      },
+    ]);
+  });
+  watcher.dispose();
+});
